c466279731fd18f38067b0a57c9d9223f83ef6c6
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2023-03-13 23:06:29 +0600

    13/3/2023

---
+++ FirstCode/src/script.ts
@@ -0,0 +1,16 @@
+const std_name = "Aubdur Rob Anik";
+console.log("Name : ", std_name);
+
+let std_id = "18101073";
+console.log("ID : ", std_id);
+
+const arrFunction = () => {
+  let count = 1;
+  for (var i = 1; i <= 12; i++) count *= i;
+
+  console.log("Value : ", count);
+};
+
+arrFunction();
+
+document.write("TypeScript Start");
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TypeScript
+
+## npm i -g typescript
+
+## npm i @types/node
+
+## tsc
+
+## tsc --init
+
+# Watch Mode
+## tsc -w
